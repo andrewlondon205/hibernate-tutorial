763f1152d00a995366acc37860e52d4a494de4bf
Ramai Alejandria <vyasadev931@gmail.com>
2022-11-28 06:40:55 +0100

    Class implementations to establish a one-to-one mapping

---
+++ OneToOneMapping/src/configuration/DBConnection.java
@@ -0,0 +1,24 @@
+package configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false";
+        String username = "softstrategy";
+        String password = "changeme";
+
+        try {
+            System.out.println("Connecting to the database");
+            Connection connection = DriverManager.getConnection(url,username,password);
+            System.out.println("Connection Successful");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
