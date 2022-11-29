058138c88b0ea09ec123d16da80a49a7227074c5
Ramai Alejandria <vyasadev931@gmail.com>
2022-11-29 21:00:44 +0100

    further implementations

--- OneToOneMapping/src/entity/Instructor.java
+++ OneToOneMapping/src/entity/Instructor.java
@@ -1,5 +1,27 @@
 package entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="instructor")
 public class Instructor {
+
+    // annotate the class as an entity and map to db table
+
+    // define the fields
+
+    // annotate the fields with db column names
+
+    // create constructors
+
+    // generate getter/setter methods
+
+    // generate toString() method
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
 
 }
